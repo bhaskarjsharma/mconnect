6877894867104e21000de99b7bf69ddd1ac9fb06
BhaskarJSharma <bhaskarj.sharma@bcplindia.co.in>
2022-02-25 11:23:43 +0530

    25.02.2022

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,9 +11,9 @@ import Flutter
         if #available(iOS 10.0, *) {
               UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
             }
-        FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+/*         FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
           GeneratedPluginRegistrant.register(with: registry)
-      }
+      } */
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
